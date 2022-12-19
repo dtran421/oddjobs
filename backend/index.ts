82a56ad44f5f17d4126172455e71f60954de74f9
dtran <duketran2001@gmail.com>
2022-12-19 13:43:26 -0500

    db setup

---
+++ backend/index.ts
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import fastify from "fastify";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./lib/database.types";
+
+const supabaseUrl =
+    process.env.NODE_ENV === "dev"
+        ? (process.env.SUPABASE_URL_DEV as string)
+        : (process.env.SUPABASE_URL_PROD as string);
+
+const supabaseKey =
+    process.env.NODE_ENV === "dev"
+        ? (process.env.SUPABASE_KEY_DEV as string)
+        : (process.env.SUPABASE_KEY_PROD as string);
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+const server = fastify();
+
+server.get("/ping", async (request, response) => {
+    return "pong\n";
+});
+
+server.listen({ port: 8000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
+
+export { supabase, server };
