d365e7180db3d4b81a82fc2cf5dde7c51f64ecda
dtran <duketran2001@gmail.com>
2022-12-24 19:49:25 -0500

    updated main readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # oddjobs
+
+A mobile app for people seeking part-time, gig (odd) jobs from well-established businesses and companies.
+
+## Features
+
+Initial release:
+
+- [X] Companies post jobs
+- [X] People sign up for jobs
+- [ ] Geolocating jobs
+
+Later releases: 
+
+- [ ] No overlapping shifts
+- [ ] Punishment for cancelling within 24 hours
+- [ ] Search bar (for browsing)
+- [ ] Survey users on signup
+- [ ] Algorithm for matching shifts
+
+## Future Plans:
+
+- Adopt [doppler](https://docs.doppler.com/docs/install-cli)
+- Use a Logo API 
+    - [Brandfetch](https://brandfetch.com/developers/logo-api)
+    - [Clearbit](https://clearbit.com/logo)
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -15,13 +15,13 @@ export const formatShiftLength = (shiftLengthTimestamp: string): string => {
     const [hours, minutes, seconds] = shiftLengthTimestamp.split(":");
 
     const shiftLength = moment.duration({
-        hours: parseInt(hours),
-        minutes: parseInt(minutes),
-        seconds: parseInt(seconds)
+        hours: parseInt(hours, 10),
+        minutes: parseInt(minutes, 10),
+        seconds: parseInt(seconds, 10)
     });
 
-    const hrs = shiftLength.hours(),
-        mins = shiftLength.minutes();
+    const hrs = shiftLength.hours();
+    const mins = shiftLength.minutes();
 
     let formattedShiftLength = `${hrs} hour${hrs > 1 ? "s" : ""}`;
     if (mins) formattedShiftLength += ` ${mins} mins`;
